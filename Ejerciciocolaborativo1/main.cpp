f7adb85009ae7b7b971df3eacf216ae212b10781
JuanCordova16 <jecordova5@espe.edu.ec>
2025-04-25 10:17:08 -0500

    Resta subida

--- Ejerciciocolaborativo1/main.cpp
+++ Ejerciciocolaborativo1/main.cpp
@@ -2,10 +2,11 @@
 
 using namespace std;
 void sumar();
-
+void restar ();
 int main()
 {
     sumar();
+    restar();
     return 0;
 }
 
@@ -17,4 +18,13 @@ cout<<"Ingrese el segundo numero"<<endl;
 cin>>b;
 c=a+b;
 cout<<"La suma es "<< c << endl;
+}
+void restar(){
+int n1, n2, n3;
+cout<<"Ingrese los numeros que va a restar"<<endl;
+cin>>n1;
+cin>>n2;
+n3= n1-n2;
+cout<<"El resultado de la resta es: "<<n3<<endl;
+
 }
